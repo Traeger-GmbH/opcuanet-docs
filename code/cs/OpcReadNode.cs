ea1d3c8a2607d346d89ec97f52713112c4e8c2b3
d.scharnagl <d.scharnagl@traeger.de>
2020-04-21 22:49:48 +0200

    Added OpcReadNode.cs

---
+++ code/cs/OpcReadNode.cs
@@ -0,0 +1,24 @@
+// DOC
+OpcValue isRunning = client.ReadNode("ns=2;s=Machine/IsRunning");
+
+// DOC
+OpcReadNode[] commands = new OpcReadNode[] {
+    new OpcReadNode("ns=2;s=Machine/Job/Number"),
+    new OpcReadNode("ns=2;s=Machine/Job/Name"),
+    new OpcReadNode("ns=2;s=Machine/Job/Speed")
+};
+
+// DOC
+IEnumerable<OpcValue> job = client.ReadNodes(commands);
+
+// DOC
+OpcValue isRunningDisplayName = client.ReadNode("ns=2;s=Machine/IsRunning", OpcAttribute.DisplayName);
+
+// DOC
+OpcReadNode[] commands = new OpcReadNode[] {
+    new OpcReadNode("ns=2;s=Machine/Job/Number", OpcAttribute.DisplayName),
+    new OpcReadNode("ns=2;s=Machine/Job/Name", OpcAttribute.DisplayName),
+    new OpcReadNode("ns=2;s=Machine/Job/Speed", OpcAttribute.DisplayName)
+};
+ 
+IEnumerable<OpcValue> jobDisplayNames = client.ReadNodes(commands);
