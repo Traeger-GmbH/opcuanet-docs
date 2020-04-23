bf8e1976a98245abcb193a7d0327ca7ffe90018a
d.scharnagl <d.scharnagl@traeger.de>
2020-04-23 13:54:28 +0200

    Added additional code to OpcClient.Licensing.cs/.vb

--- code/cs/OpcClient.Licensing.cs
+++ code/cs/OpcClient.Licensing.cs
@@ -10,3 +10,10 @@ ILicenseInfo license = Opc.UaFx.Client.Licenser.LicenseInfo;
 if (license.IsExpired)
     Console.WriteLine("The OPA UA Framework Advanced license is expired!");
 
+
+// DOC
+#if DEBUG
+    Opc.UaFx.Client.Licenser.FailIfUnlicensed();
+#else
+    Opc.UaFx.Client.Licenser.ThrowIfUnlicensed();
+#endif
--- code/vb/OpcClient.Licensing.vb
+++ code/vb/OpcClient.Licensing.vb
@@ -10,3 +10,10 @@ Dim license As ILicenseInfo = Opc.UaFx.Client.Licenser.LicenseInfo
 If license.IsExpired Then
     Console.WriteLine("The OPA UA Framework Advanced license is expired!")
 End If
+
+'DOC
+#If DEBUG Then
+    Opc.UaFx.Client.Licenser.FailIfUnlicensed()
+#Else
+    Opc.UaFx.Client.Licenser.ThrowIfUnlicensed()
+#End If
