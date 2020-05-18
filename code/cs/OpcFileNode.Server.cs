d588001e5f1fdd6b1cb12c754c19334528532897
d.scharnagl <d.scharnagl@traeger.de>
2020-05-18 08:33:44 +0200

    Added OpcFileNode.Server.cs/.vb

---
+++ code/cs/OpcFileNode.Server.cs
@@ -0,0 +1,5 @@
+// DOC
+var protocollFileNode = new OpcFileNode(
+        machineNode,
+        "Protocol.txt",
+        new FileInfo(@"..\Protocol.log"));
---
+++ code/vb/OpcFileNode.Server.vb
@@ -0,0 +1,5 @@
+'DOC
+Dim protocollFileNode = New OpcFileNode( _
+        machineNode, _
+        "Protocol.txt", _
+        New FileInfo("..\Protocol.log"))
