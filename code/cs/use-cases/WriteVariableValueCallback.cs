ab96c2fc61712e2cbdfc519ac39612210df6827b
Dominik Scharnagl <48177425+dscharnagl@users.noreply.github.com>
2020-06-15 13:35:51 +0200

    Create WriteVariableValueCallback.cs

---
+++ code/cs/use-cases/WriteVariableValueCallback.cs
@@ -0,0 +1,27 @@
+this.variableNode.WriteVariableValueCallback = this.WriteToNode;
+
+// Deny write of a specific type of value.
+private OpcVariableValue WriteToNode(
+        OpcWriteVariableValueContext context,
+        OpcVariableValue value)
+{
+    if (!(value.Value is short))
+        value.Status.Update(OpcStatusCode.BadTypeMismatch);
+
+    return value;
+}
+
+// Inline change type of value to the expected type.
+private OpcVariableValue WriteToNode(
+    OpcWriteVariableValueContext context,
+    OpcVariableValue value)
+{
+    if (!(value.Value is short)) {
+        value = new OpcVariableValue(
+                Convert.ChangeType(value.Value, typeof(short)),
+                value.Timestamp ?? DateTime.UtcNow,
+                value.Status);
+    }
+
+    return value;
+}
