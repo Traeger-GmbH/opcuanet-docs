8706f5750789d60427bc44e3fae36dc327e18b1a
Dominik Scharnagl <48177425+dscharnagl@users.noreply.github.com>
2020-06-16 09:52:59 +0200

    Update SingletonSession.cs

--- code/cs/use-cases/SingletonSession.cs
+++ code/cs/use-cases/SingletonSession.cs
@@ -1,12 +1,31 @@
+server.SessionActivated += (sender, e) => {
+    var identityEntry = GetUserEntry(server.Security, e.Session);
+
+    if (identityEntry != null)
+        identityEntry.Disable(OpcEndpointIdentity.GetCurrent());
+};
+
 server.SessionClosing += (sender, e) => {
     var identityEntry = GetUserEntry(server.Security, e.Session);
 
-    if (identityEntry != null) {
-        var endpoint = OpcEndpointIdentity.GetCurrent();
-
-        if (endpoint == null)
-            identityEntry.Enable(identityEntry.DisabledEndpoints);
-        else
-            identityEntry.Enable(endpoint);
-    }
+    if (identityEntry != null)
+        identityEntry.Enable(OpcEndpointIdentity.GetCurrent());
 };
+
+
+private static OpcAccessControlEntry GetUserEntry(
+        OpcServerSecurity security,
+        OpcSession session)
+{
+    var identity = session.UsedIdentity;
+
+    // In case of anonymous.
+    if (identity == null)
+        return null;
+
+    return (from userEntry in security.UserNameAcl.Entries
+            let userPrincipal = userEntry.Principal
+            let userIdentity = (OpcUserIdentity)userPrincipal.Identity
+            where userIdentity.DisplayName == identity.DisplayName
+            select userEntry).FirstOrDefault();
+}
